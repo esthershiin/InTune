0b9372ef8367262f7011b98fdf9b6b61a4297b4c
Esther Shin <esthershiin@berkeley.edu>
2020-04-24 14:29:30 -0700

    working on storyboard

--- InTune/SceneDelegate.swift
+++ InTune/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if isLoggedIn {
-            let vc = storyboard.instantiateViewController (withIdentifier: "ProfileVC") as! ProfileViewController
+            let vc = storyboard.instantiateViewController(withIdentifier: "ProfileVC") as! ProfileViewController
             self.window = UIWindow(windowScene: windowScene)
             self.window?.rootViewController = vc
             self.window?.makeKeyAndVisible()
