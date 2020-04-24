c3e752a4e843596b4d67d1f9633488da4cbbaa0f
Allyson Park <allysonpark@berkeley.edu>
2020-04-23 17:00:20 -0700

    created relevant viewcontroller swift files. also created model files, not sure if they're relevant, might want to change model structure

---
+++ InTune/Model/Tuner.swift
@@ -0,0 +1,13 @@
+//
+//  Tuner.swift
+//  InTune
+//
+//  Created by Allyson on 4/23/20.
+//
+//  Tuner contains the logic to generate compability
+//  data between two users. It accesses the Spotify user
+//  data for both users–specifically their top tracks and
+//  top artists over the past 6 months. It uses the Spotify
+//  API to do so. 
+
+import Foundation
---
+++ InTune/Model/UserData.swift
@@ -0,0 +1,10 @@
+//
+//  UserData.swift
+//  InTune
+//
+//  Created by Allyson on 4/23/20.
+//
+
+/* Notes for development: Will probably need an abstraction for a user (class or struct?). The class will need a username, name, date joined, top match, average match score, and maybe a data structure that holds all the matches. */
+
+import Foundation
