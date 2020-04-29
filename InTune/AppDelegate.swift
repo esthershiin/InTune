b54e9b8feabf0c285721f2b79697fe937bf12bcf
Allyson Park <allysonpark@berkeley.edu>
2020-04-29 01:01:31 -0700

    Added some stuff to the appdelegate and userdata in accordance with the advanced authorization instructions. Currently still figuring out how to do user authorization.

--- InTune/AppDelegate.swift
+++ InTune/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 import Firebase
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate {
 
 
     /* EDIT MADE BY ALLYSON: Change initial view controller based off login data */
@@ -31,6 +31,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    //THE THREE FUNCTIONS BELOW ARE COPIED FROM THE SPOTIFY SDK INSTRUCTIONS FOR ADVANCED AUTHORIZATION 
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+      print("success", session)
+    }
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+      print("fail", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+      print("renewed", session)
     }
 
 
--- InTune/Model/UserData.swift
+++ InTune/Model/UserData.swift
@@ -8,10 +8,30 @@
 /* Notes for development: Will probably need an abstraction for a user (class or struct?). The class will need a username, name, date joined, top match, average match score, and maybe a data structure that holds all the matches. */
 
 import Foundation
-import SpotifyiOS.h
+import SpotifyiOS
 
 var isLoggedIn: Bool = true
 
-var configuration: SPTConfiguration = [[SPTConfiguration, alloc] initWithClientID:@"your_client_id" redirectURL:[NSURL urlWithString:@"your_redirect_uri"]];
+let SpotifyClientID = "b29fa2b4649e4bc697ecbf6721edaa39"
 
+//should the redirectURL be a custom url? should it be a universal link? could it be localhost? on the spotify developers page and in the plist i temporarily listed it as localhost:8888/callback 
+let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+
+lazy var configuration = SPTConfiguration(
+  clientID: SpotifyClientID,
+  redirectURL: SpotifyRedirectURL
+)
+
+let tokenSwapURLString = "https://spotify-token-swap.glitch.me/api/token"
+guard let tokenSwapURL = URL(string: tokenSwapURLString) else {return}
+
+URLSession.shared.dataTask(with: url) { (data, response, err) in
+    guard let tokenData = data else { return }
+    let jsonTokenData = try? JSONSerialization.jsonObject(with: userData, options: [])
+    guard let dictionary = jsonTokenData as? [String: Any] else { return }
+    guard let access_token = dictionary["access_token"] else { return }
+    guard let expires_in = dictionary["expires_in"] else { return }
+    guard let refresh_token = dictionary["refresh_token"] else { return }
+    guard let scope = dictionary["scope"] else { return }
+}.resume()
 
